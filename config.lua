2281e9cfe6d3e4f3cd089cbf94e06b24055feeed
Rayaan Uddin <91953921+RayaanUddin@users.noreply.github.com>
2024-04-15 01:03:38 +0100

    Updated Notify

--- config.lua
+++ config.lua
@@ -2,30 +2,21 @@ Config = {}
 
 Config.Debug = true -- Set to false to disable debug messages
 Config.Framework = "qb-core" -- "qb-core" or "es_extended"
-Config.Target = "none" -- "none" or "ox_target" or "qb-target", etc
-Config.Notify = "qb"
+Config.Target = "qb-target" -- "none" or "ox_target" or "qb-target", etc
+Config.Menu = { -- or other(Update the menu.lua file)
+	Menu = "qb", -- "ox" or "qb"
+	Input = "ox", -- "ox" or "qb"
+} -- If you do not have Ox_lib, remove it from fxmanifest.lua
 
 --- Funtion to notify user
 ---@param: message: The message you want to send to user
 ---@param: type: The type of notification you want to send to user
 ---@param: src: The source of the player you want to send notification to
 Config.triggerNotify = function(message, type, src)
-    if Config.Notify == "okok" then
-		if not src then	exports['okokNotify']:Alert(title, message, 6000, type)
-		else TriggerClientEvent('okokNotify:Alert', src, title, message, 6000, type) end
-	elseif Config.Notify == "qb" then
-		if not src then	TriggerEvent("QBCore:Notify", message, type)
-		else TriggerClientEvent("QBCore:Notify", src, message, type) end
-	elseif Config.Notify == "t" then
-		if not src then exports['t-notify']:Custom({title = title, style = type, message = message, sound = true})
-		else TriggerClientEvent('t-notify:client:Custom', src, { style = type, duration = 6000, title = title, message = message, sound = true, custom = true}) end
-	elseif Config.Notify == "infinity" then
-		if not src then TriggerEvent('infinity-notify:sendNotify', message, type)
-		else TriggerClientEvent('infinity-notify:sendNotify', src, message, type) end
-	elseif Config.Notify == "rr" then
-		if not src then exports.rr_uilib:Notify({msg = message, type = type, style = "dark", duration = 6000, position = "top-right", })
-		else TriggerClientEvent("rr_uilib:Notify", src, {msg = message, type = type, style = "dark", duration = 6000, position = "top-right", }) end
-	end
+    --[[if not src then	exports['okokNotify']:Alert(title, message, 6000, type)
+	else TriggerClientEvent('okokNotify:Alert', src, title, message, 6000, type) end]]
+	if not src then	TriggerEvent("QBCore:Notify", message, type)
+	else TriggerClientEvent("QBCore:Notify", src, message, type) end
 end
 
 --- Function to show UI to open stash in its zone
